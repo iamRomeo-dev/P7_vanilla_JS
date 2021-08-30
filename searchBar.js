e2b08368c449882748e4de793c488dab08fc06af
iamRomeo-dev <romeo.brilland@live.fr>
2021-08-30 20:44:00 +0200

    the same

--- searchBar.js
+++ searchBar.js
@@ -3,29 +3,26 @@ import { renderRecipes } from "./app.js";
 
 const $recipeList = document.getElementById("recipes");
 
-export const searchBar = (filteredArray) => {
-  const $searchBar = document.getElementById("searchBar");
-
-  $searchBar.addEventListener("keyup", (event) => {
-    const searchString = event.target.value.toLowerCase();
-    if (searchString.length > 2) {
-      const filteredSearch = filteredArray.filter((recipe) => {
-        return (
-          recipe.name.toLowerCase().includes(searchString) ||
-          recipe.description.toLowerCase().includes(searchString) ||
-          recipe.ingredients
-            .map((ingredient) => ingredient.ingredient.toLowerCase())
-            .includes(searchString)
-        );
-      });
-      if (filteredSearch.length > 0) {
-      renderRecipes(filteredSearch);
-      } else {
-        $recipeList.innerHTML = "Aucune recette ne correspond à votre critère...";
-      }
-    }
-    if (searchString === "") {
-      renderRecipes(filteredArray);
-    }
+export const searchBar = (filteredArray, searchString) => {
+  const filteredSearch = filteredArray.filter((recipe) => {
+    return (
+      recipe.name.toLowerCase().includes(searchString) ||
+      recipe.description.toLowerCase().includes(searchString) ||
+      recipe.ingredients
+        .map((ingredient) => ingredient.ingredient.toLowerCase())
+        .includes(searchString)
+    );
   });
+
+  if (filteredSearch.length > 0) {
+    console.log(888888, filteredSearch);
+    return filteredSearch;
+  } else {
+    $recipeList.innerHTML = "Aucune recette ne correspond à votre critère...";
+  }
+
+  if (searchString === "") {
+    return 25;
+    // renderRecipes(filteredArray);
+  }
 };
